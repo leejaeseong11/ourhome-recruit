dace7655106da7c1cf4ec653ad685bb3983c2796
Jaeseong <easeon78@gmail.com>
2024-01-01 02:31:21 +0900

    feat: product paginate

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,8 +1,29 @@
+import { useState } from 'react';
+import Product from './components/Product.jsx';
+import ProductData from '../../data/product';
+import Pagination from './components/Pagination.jsx';
 // 상품 페이지
 const Main = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+
   return (
     <>
-      <div>main</div>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={ProductData.length}
+        onPageChange={setCurrentPage}
+      />
+      {ProductData.slice(currentPage - 1, currentPage + 7).map(
+        (product, index) => {
+          return (
+            <Product
+              key={product.productId}
+              product={product}
+              order={index + 1 + (currentPage - 1) * 8}
+            />
+          );
+        }
+      )}
     </>
   );
 };
